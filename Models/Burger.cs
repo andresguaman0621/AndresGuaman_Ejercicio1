70619f6fbb30d6e10cbd0613dbb784d5c240a7c0
andresguaman0621 <130424144+andresguaman0621@users.noreply.github.com>
2023-10-11 09:27:37 -0500

    Update Burger.cs

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -3,9 +3,15 @@ namespace AndresGuaman_Ejercicio1.Models
 {
 	public class Burger
 	{
-		public Burger()
-		{
-		}
+		
+
+        public int Id { get; set; }
+        
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+
+       
+        public decimal Precio { get; set; }
 	}
 }
 

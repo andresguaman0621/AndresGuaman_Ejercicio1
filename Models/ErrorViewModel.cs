b8ee80998b152f83e8df38cd3c074105f2862da8
andresguaman0621 <130424144+andresguaman0621@users.noreply.github.com>
2023-10-11 08:14:03 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AndresGuaman_Ejercicio1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

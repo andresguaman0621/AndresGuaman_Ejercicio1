f52c81b72105312334496308c513edeb5ba8c5fd
andresguaman0621 <130424144+andresguaman0621@users.noreply.github.com>
2023-10-11 09:30:00 -0500

    Clase burger final

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,16 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AndresGuaman_Ejercicio1.Models
 {
 	public class Burger
 	{
-		
-
-        public int Id { get; set; }
-        
+	    public int Id { get; set; }
         public string? Name { get; set; }
         public bool WithCheese { get; set; }
-
-       
+        [Range(0.01, 9999.99)]
         public decimal Precio { get; set; }
 	}
 }
